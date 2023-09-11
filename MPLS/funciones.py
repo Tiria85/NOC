f06c594c64e549dbccdfc6b636fa96e0966b4521
Arturo Tiria <arturotiriajp@gmail.com>
2023-09-10 20:26:18 -0500

    Agregando crpeta courcera inicio de Django

--- MPLS/funciones.py
+++ MPLS/funciones.py
@@ -21,3 +21,30 @@ buscar_palabras(archivo_entrada, archivo_salida, palabras_clave)
 print("Proceso completado. Líneas encontradas han sido guardadas en", archivo_salida)
 
 # actualizando Git
+
+
+import tkinter as tk
+from tkinter import filedialog
+
+def cargar_archivo():
+    archivo_ruta = filedialog.askopenfilename(filetypes=[("Archivos de texto", "*.log")])
+    if archivo_ruta:
+        entry_ruta.delete(0, tk.END)  # Limpiamos el campo de texto
+        entry_ruta.insert(0, archivo_ruta)
+
+# Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Cargar Archivo")
+
+# Crear y posicionar los widgets
+etiqueta = tk.Label(ventana, text="Seleccione un archivo:")
+etiqueta.pack(pady=10)
+
+entry_ruta = tk.Entry(ventana, width=40)
+entry_ruta.pack()
+
+boton_cargar = tk.Button(ventana, text="Cargar Archivo", command=cargar_archivo)
+boton_cargar.pack(pady=10)
+
+# Iniciar el bucle de eventos
+ventana.mainloop()
