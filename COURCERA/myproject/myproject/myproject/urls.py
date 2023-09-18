7bbcbe063e2bc11db409017b6a0af8613dc7b063
Arturo Tiria <arturotiriajp@gmail.com>
2023-09-18 03:07:43 -0500

    nuevo pryecto Django

---
+++ COURCERA/myproject/myproject/myproject/urls.py
@@ -0,0 +1,25 @@
+"""
+Configuración de URL para el proyecto myproject.
+
+La lista `urlpatterns` dirige las URL a las vistas. Para obtener más información, consulte:
+     https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Ejemplos:
+Vistas de funciones
+     1. Agregue una importación: desde las vistas de importación de my_app
+     2. Agregue una URL a urlpatterns: ruta('', views.home, nombre='home')
+Vistas basadas en clases
+     1. Agregue una importación: desde other_app.views import Inicio
+     2. Agregue una URL a urlpatterns: ruta('', Home.as_view(), nombre='home')
+Incluyendo otra URLconf
+     1. Importe la función include(): desde django.urls importe include, ruta
+     2. Agregue una URL a urlpatterns: ruta('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', views.home),
+    path('', views.index),
+]
