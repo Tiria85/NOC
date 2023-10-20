7c63c8da50deeceef895d24bfeed43d3abb4314a
Tiria85 <136491628+Tiria85@users.noreply.github.com>
2023-10-19 21:09:57 -0500

    Agregado base de datos

--- COURCERA/myproject/myproject/myapp/models.py
+++ COURCERA/myproject/myproject/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Bebidas(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
