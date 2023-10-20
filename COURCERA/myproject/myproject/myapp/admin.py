ceab795047f96367beb0ca63da70305b62f585f2
Tiria85 <136491628+Tiria85@users.noreply.github.com>
2023-10-20 01:26:49 -0500

    na

--- COURCERA/myproject/myproject/myapp/admin.py
+++ COURCERA/myproject/myproject/myapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Bebidas
+from .models import CategoriaDeBebidas
 
 # Register your models here.
 
 admin.site.register(Bebidas)
+admin.site.register(CategoriaDeBebidas)
--- COURCERA/myproject/myproject/myapp/models.py
+++ COURCERA/myproject/myproject/myapp/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Bebidas(models.Model):
-    nombre = models.CharField(max_length=200)
-    descripcion = models.TextField()
+    nombre_bebida = models.CharField(max_length=200)
+    descripcion = models.TextField(blank=True, null=True, help_text="Descripción de la categoría")
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey("CategoriaDeBebidas", on_delete=models.SET_NULL, null=True, blank=True)
+    #relación de uno a muchos entre CategoriaDeBebidas y Bebidas
+
+class CategoriaDeBebidas(models.Model):
+    nombre = models.CharField(max_length=100, unique=True, help_text="Nombre de la categoría")
+    descripcion = models.TextField(blank=True, null=True, help_text="Descripción de la categoría")
+    #bebida = models.ForeignKey('Bebidas', on_delete=models.SET_NULL, null=True, blank=True)
+    
+
 
